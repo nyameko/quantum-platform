00b6486b6d917e6d7e0071eb4fcfcde7a37c75f4
Nyameko Lisa <connect@nyameko.com>
2026-09-14 21:50:59 +0200

    UPdated astro.config.mjs

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+    site: 'https://blog.nyameko.com',
     server: {
-        host: true,
+        host: '127.0.0.1',
     },
 });
