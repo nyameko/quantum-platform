7ece7b3f4eb3ccec8a1f4a047cd5d83f4aaed4c9
Nyameko Lisa <connect@nyameko.com>
2026-09-15 16:16:26 +0200

    Added Vite development proxy to astro users

--- apps/users/astro.config.mjs
+++ apps/users/astro.config.mjs
@@ -1,8 +1,25 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://blog.nyameko.com',
+    site: 'https://users.quantum.nyameko.com',
+
     server: {
         host: '127.0.0.1',
+    },
+
+    vite: {
+        server: {
+            proxy: {
+                '/_allauth': {
+                    target: 'http://127.0.0.1:8000',
+                    changeOrigin: true,
+                },
+
+                '/api/v1': {
+                    target: 'http://127.0.0.1:8000',
+                    changeOrigin: true,
+                },
+            },
+        },
     },
 });
